53e69967736ba83d1a5fde6afc15b79c9f0d9d0d
Steven Holms <superlinkx@gmail.com>
2014-12-14 12:49:18 -0800

    Added engine loop and canvas definition

--- game/engine.js
+++ game/engine.js
@@ -3,14 +3,31 @@ var engine = (function() {
   //Create the engine object
   //Private properties and methods
   var entityCount = 0; //Keeps track of how many entities have been created so far
+  var engineStart;
+  var engineLoop;
+  var loop = function(timestamp) {
+    engineLoop = window.requestAnimationFrame(loop);
+    var progress = timestamp - engineStart;
+    engineObj.runSystems();
+  };
 
-  //Public properties and methods
-  return {
+  var engineObj = {
     entities: {}, //Table to contain engine entities
     components: {}, //Table of components
     systems: {}, //Table of systems
-    init: function() {
+    init: function(w, h, canvasEl) {
       //Initialization stuff
+      //Takes a width, height, and optional canvas id (id defaults to game)
+      var canvas = document.getElementById(canvasEl || 'game').getContext('2d');
+      canvas.width = w || 720;
+      canvas.height = h || 480;
+      engineStart = Date.now();
+
+      engineLoop = window.requestAnimationFrame(loop);
+    },
+    stop: function() {
+      if(engineLoop)
+        window.cancelAnimationFrame(engineLoop);
     },
     getNewEntityId: function() {
       //Method to return a new entity id
@@ -24,4 +41,7 @@ var engine = (function() {
       }
     }
   };
+
+  //Public properties and methods
+  return engineObj;
 }());
